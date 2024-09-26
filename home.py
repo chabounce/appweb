09b9bbf200ae4abfae376541e13e0f0d2fa32fe2
chabounce <charlotte.jeun@eemi.com>
2024-09-26 14:22:10 +0200

    Update home.py

--- home.py
+++ home.py
@@ -10,3 +10,6 @@ st.write("Ceci est un formulaire de contact")
 user_input = st.text_input("Tapez votre texte :")
 
 st.write(user_input)
+
+#Image
+st.image("https://fac.img.pmdstatic.net/fit/~1~fac~2021~12~20~db44c7df-7828-498b-821f-4bd03cdfb75d.jpeg/1200x900/quality/80/crop-from/center/tout-savoir-sur-l-orque.jpeg")
