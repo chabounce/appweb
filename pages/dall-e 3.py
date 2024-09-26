0d55d438d2115bb7feb78adf570a5efdbf5951bf
chabounce <charlotte.jeun@eemi.com>
2024-09-26 15:26:37 +0200

    Update dall-e 3.py

--- pages/dall-e 3.py
+++ pages/dall-e 3.py
@@ -4,3 +4,6 @@ st.title("Dall-e 3")
 
 user_input = st.text_input("Application Web - Open IA")
 st.write(user_input)
+
+sidebar_input = st.sidebar.text_input("Tapez votre texte ici :")
+st.write(sidebar_input)
