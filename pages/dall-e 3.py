e1efd2f175d1ce4dfaddd7f2b047ebdbb1ef20b6
chabounce <charlotte.jeun@eemi.com>
2024-09-26 15:31:46 +0200

    Update dall-e 3.py

--- pages/dall-e 3.py
+++ pages/dall-e 3.py
@@ -7,3 +7,19 @@ st.write(user_input)
 
 sidebar_input = st.sidebar.text_input("Tapez votre texte ici :")
 st.write(sidebar_input)
+
+from openai import OpenAI
+client = OpenAI(api_key=OpenAIKEY)
+
+prompt = "A cute baby sea otter"
+
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=prompt,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+image_url = image.data[0].url
+
+st.image_url
