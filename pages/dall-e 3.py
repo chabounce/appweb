09de278ff2e93e663c5ca410708de8fe82d0387c
chabounce <charlotte.jeun@eemi.com>
2024-09-26 15:24:50 +0200

    Update dall-e 3.py

--- pages/dall-e 3.py
+++ pages/dall-e 3.py
@@ -2,7 +2,5 @@ import streamlit as st
 
 st.title("Dall-e 3")
 
-st.write("Application Web - Open IA")
-
-user_input = st.text_input("Tapez votre texte :")
+user_input = st.text_input("Application Web - Open IA")
 st.write(user_input)
